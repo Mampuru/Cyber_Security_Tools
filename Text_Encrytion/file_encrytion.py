75105919229aa75de3f0122657c76a6f3489eff1
Mampuru <mampuruntwampe@gmail.com>
2023-12-17 23:55:48 +0200

    Addition of function that encrypts the file contents

--- Text_Encrytion/file_encrytion.py
+++ Text_Encrytion/file_encrytion.py
@@ -4,3 +4,16 @@ from cryptography.fernet import Fernet
 def generate_key():
     return Fernet.generate_key()
 
+# Function that will take a file and encrypt the contents of the file
+def encrypt_file(file_path, key):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+
+    f = Fernet(key)
+    encrypted_data = f.encrypt(data)
+
+    encrypted_file_path = file_path + '.encrypted'
+    with open(encrypted_file_path, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted_data)
+
+    return encrypted_file_path
