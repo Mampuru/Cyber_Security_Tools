f5ddf9559a97ee7b6cac29af068b083542471e47
Mampuru <mampuruntwampe@gmail.com>
2023-12-17 23:57:39 +0200

    Addition of the decrytion function

--- Text_Encrytion/file_encrytion.py
+++ Text_Encrytion/file_encrytion.py
@@ -17,3 +17,17 @@ def encrypt_file(file_path, key):
         encrypted_file.write(encrypted_data)
 
     return encrypted_file_path
+
+# Function that will decrypt the file content once correct key is supplied
+def decrypt_file(encrypted_file_path, key):
+    with open(encrypted_file_path, 'rb') as encrypted_file:
+        encrypted_data = encrypted_file.read()
+
+    f = Fernet(key)
+    decrypted_data = f.decrypt(encrypted_data)
+
+    decrypted_file_path = encrypted_file_path[:-len('.encrypted')]
+    with open(decrypted_file_path, 'wb') as decrypted_file:
+        decrypted_file.write(decrypted_data)
+
+    return decrypted_file_pat
