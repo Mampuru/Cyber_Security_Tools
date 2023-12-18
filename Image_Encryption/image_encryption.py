8d1ace39047d97f3896bd9458efe5cdeaba48782
Mampuru <mampuruntwampe@gmail.com>
2023-12-18 22:13:15 +0200

    Addition of the decryption method

--- Image_Encryption/image_encryption.py
+++ Image_Encryption/image_encryption.py
@@ -19,3 +19,18 @@ def encrypt_image(image_path, key):
     encrypted_image.save(encrypted_image_path)
 
     return encrypted_image_path
+
+# function that takes an image and decrypts it 
+def decrypt_image(encrypted_image_path, key):
+    with open(encrypted_image_path, 'rb') as encrypted_image_file:
+        encrypted_image_data = encrypted_image_file.read()
+
+    f = Fernet(key)
+    decrypted_data = f.decrypt(encrypted_image_data)
+
+    original_format = encrypted_image_path.split('.')[-1]
+    decrypted_image = Image.open(io.BytesIO(decrypted_data))
+    decrypted_image_path = encrypted_image_path.split('_encrypted')[0] + '_decrypted.' + original_format
+    decrypted_image.save(decrypted_image_path)
+
+    return decrypted_image_path
